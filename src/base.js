a3966cdbda57eab1c90a2cfde2a68c57c64219f5
g11v8a <g11v8a@gmail.com>
2018-03-19 15:27:41 +0800

    :rocket:

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyCIOPG_ckdm_v35RWABYm9GeemLW2FmGI8',
+  authDomain: 'catch-of-the-day-c686c.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-c686c.firebaseio.com'
+});
+
+export default base;
